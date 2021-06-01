433b24bb3802c41461d7b3bdc974ba33647f2d67
William Reis Fernandes <william@mapall.com>
2021-06-01 20:41:55 +0100

    feat: tenant and fixes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # First-Chrome-Extension
+
+
+
+# URL
+https://stackoverflow.com/questions/11684454/getting-the-source-html-of-the-current-page-from-chrome-extension
+
+
+
+https://stackoverflow.com/questions/56451731/dom-parser-chrome-extension-memory-leak
--- script.js
+++ script.js
@@ -1,70 +1,221 @@
 // import $ from "jquery";
 
-const paises = 
-[
-    'África do Sul','Albânia','Alemanha','Andorra','Angola','Anguilla','Antigua','Arábia Saudita','Argentina','Armênia','Aruba','Austrália','Áustria',
-    'Azerbaijão','Bahamas','Bahrein','Bangladesh','Barbados','Bélgica','Benin','Bermudas','Botsuana','Brasil','Brunei','Bulgária','Burkina Fasso','botão',
-    'Cabo Verde','Camarões','Camboja','Canadá','Cazaquistão','Chade','Chile','China','Cidade do Vaticano','Colômbia','Congo','Coréia do Sul','Costa do Marfim',
-    'Costa Rica','Croácia','Dinamarca','Djibuti','Dominica','EUA','Egito','El Salvador','Emirados Árabes','Equador','Eritréia','Escócia','Eslováquia','Eslovênia',
-    'Espanha','Estônia','Etiópia','Fiji','Filipinas','Finlândia','França','Gabão','Gâmbia','Gana','Geórgia','Gibraltar','Granada','Grécia','Guadalupe','Guam','Guatemala',
-    'Guiana','Guiana Francesa','Guiné-bissau','Haiti','Holanda','Honduras','Hong Kong','Hungria','Iêmen','Ilhas Cayman','Ilhas Cook','Ilhas Curaçao','Ilhas Marshall',
-    'Ilhas Turks & Caicos','Ilhas Virgens','Ilhas Wallis e Futuna','Índia','Indonésia','Inglaterra','Irlanda','Islândia','Israel','Itália','Jamaica','Japão','Jordânia',
-    'Kuwait','Latvia','Líbano','Liechtenstein','Lituânia','Luxemburgo','Macau','Macedônia','Madagascar','Malásia','Malaui','Mali','Malta','Marrocos','Martinica','Mauritânia',
-    'Mauritius','México','Moldova','Mônaco','Montserrat','Nepal','Nicarágua','Niger','Nigéria','Noruega','Nova Caledônia','Nova Zelândia','Omã','Palau','Panamá','Papua-nova Guiné',
-    'Paquistão','Peru','Polinésia Francesa','Polônia','Porto Rico','Portugal','Qatar','Quênia','Rep. Dominicana','Rep. Tcheca','Reunion','Romênia','Ruanda','Rússia',
-    'Saipan','Samoa Americana','Senegal','Serra Leone','Seychelles','Singapura','Síria','Sri Lanka','St. Kitts & Nevis','St. Lúcia','St. Vincent','Sudão','Suécia',
-    'Suiça','Suriname','Tailândia','Taiwan','Tanzânia','Togo','Trinidad','Tobago','Tunísia','Turquia','Ucrânia','Uganda','Uruguai','Venezuela','Vietnã','Zaire','Zâmbia','Zimbábue'
+const paises = [
+  'África do Sul',
+  'Albânia',
+  'Alemanha',
+  'Andorra',
+  'Angola',
+  'Anguilla',
+  'Antigua',
+  'Arábia Saudita',
+  'Argentina',
+  'Armênia',
+  'Aruba',
+  'Austrália',
+  'Áustria',
+  'Azerbaijão',
+  'Bahamas',
+  'Bahrein',
+  'Bangladesh',
+  'Barbados',
+  'Bélgica',
+  'Benin',
+  'Bermudas',
+  'Botsuana',
+  'Brasil',
+  'Brunei',
+  'Bulgária',
+  'Burkina Fasso',
+  'botão',
+  'Cabo Verde',
+  'Camarões',
+  'Camboja',
+  'Canadá',
+  'Cazaquistão',
+  'Chade',
+  'Chile',
+  'China',
+  'Cidade do Vaticano',
+  'Colômbia',
+  'Congo',
+  'Coréia do Sul',
+  'Costa do Marfim',
+  'Costa Rica',
+  'Croácia',
+  'Dinamarca',
+  'Djibuti',
+  'Dominica',
+  'EUA',
+  'Egito',
+  'El Salvador',
+  'Emirados Árabes',
+  'Equador',
+  'Eritréia',
+  'Escócia',
+  'Eslováquia',
+  'Eslovênia',
+  'Espanha',
+  'Estônia',
+  'Etiópia',
+  'Fiji',
+  'Filipinas',
+  'Finlândia',
+  'França',
+  'Gabão',
+  'Gâmbia',
+  'Gana',
+  'Geórgia',
+  'Gibraltar',
+  'Granada',
+  'Grécia',
+  'Guadalupe',
+  'Guam',
+  'Guatemala',
+  'Guiana',
+  'Guiana Francesa',
+  'Guiné-bissau',
+  'Haiti',
+  'Holanda',
+  'Honduras',
+  'Hong Kong',
+  'Hungria',
+  'Iêmen',
+  'Ilhas Cayman',
+  'Ilhas Cook',
+  'Ilhas Curaçao',
+  'Ilhas Marshall',
+  'Ilhas Turks & Caicos',
+  'Ilhas Virgens',
+  'Ilhas Wallis e Futuna',
+  'Índia',
+  'Indonésia',
+  'Inglaterra',
+  'Irlanda',
+  'Islândia',
+  'Israel',
+  'Itália',
+  'Jamaica',
+  'Japão',
+  'Jordânia',
+  'Kuwait',
+  'Latvia',
+  'Líbano',
+  'Liechtenstein',
+  'Lituânia',
+  'Luxemburgo',
+  'Macau',
+  'Macedônia',
+  'Madagascar',
+  'Malásia',
+  'Malaui',
+  'Mali',
+  'Malta',
+  'Marrocos',
+  'Martinica',
+  'Mauritânia',
+  'Mauritius',
+  'México',
+  'Moldova',
+  'Mônaco',
+  'Montserrat',
+  'Nepal',
+  'Nicarágua',
+  'Niger',
+  'Nigéria',
+  'Noruega',
+  'Nova Caledônia',
+  'Nova Zelândia',
+  'Omã',
+  'Palau',
+  'Panamá',
+  'Papua-nova Guiné',
+  'Paquistão',
+  'Peru',
+  'Polinésia Francesa',
+  'Polônia',
+  'Porto Rico',
+  'Portugal',
+  'Qatar',
+  'Quênia',
+  'Rep. Dominicana',
+  'Rep. Tcheca',
+  'Reunion',
+  'Romênia',
+  'Ruanda',
+  'Rússia',
+  'Saipan',
+  'Samoa Americana',
+  'Senegal',
+  'Serra Leone',
+  'Seychelles',
+  'Singapura',
+  'Síria',
+  'Sri Lanka',
+  'St. Kitts & Nevis',
+  'St. Lúcia',
+  'St. Vincent',
+  'Sudão',
+  'Suécia',
+  'Suiça',
+  'Suriname',
+  'Tailândia',
+  'Taiwan',
+  'Tanzânia',
+  'Togo',
+  'Trinidad',
+  'Tobago',
+  'Tunísia',
+  'Turquia',
+  'Ucrânia',
+  'Uganda',
+  'Uruguai',
+  'Venezuela',
+  'Vietnã',
+  'Zaire',
+  'Zâmbia',
+  'Zimbábue',
 ];
 
-
-
-
-
-let filter = document.querySelector("#search-text");
-filter.addEventListener("input", function() {
-    // console.log(this.value+"$$$$$$");
-    // let resp = []
-    // for (item of paises) {
-    //     if (item == filter.value) {
-    //         resp.push(item);
-    //     }
-    // }
-    // console.log(resp)
+let filter = document.querySelector('input[id="search-text"]');
+console.log(filter);
+filter.addEventListener('input', function () {
+  // console.log(this.value+"$$$$$$");
+  // let resp = []
+  // for (item of paises) {
+  //     if (item == filter.value) {
+  //         resp.push(item);
+  //     }
+  // }
+  // console.log(resp)
 });
 
-var botao = document.querySelector("#btn");
-botao.addEventListener("click", function(event){
-    event.preventDefault();
-    // var text = document.querySelector("#search-text");
-    // console.log("lupa clicada");
-    // let listaPaises = paises;
-    // console.log(listaPaises);
-    let resp = []
-    for (item of paises) {
-        if (item == filter.value) {
-            resp.push(item);
-        };
-    };
-    console.log(resp)
+var botao = document.querySelector('#btn');
+botao.addEventListener('click', function (event) {
+  event.preventDefault();
+  // var text = document.querySelector("#search-text");
+  // console.log("lupa clicada");
+  // let listaPaises = paises;
+  // console.log(listaPaises);
+  let resp = [];
+  for (item of paises) {
+    if (item == filter.value) {
+      resp.push(item);
+    }
+  }
+  console.log(resp);
 
-    // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    //     var url = tabs[0].url;
-    //     // use `url` here inside the callback because it's asynchronous!
+  // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+  //     var url = tabs[0].url;
+  //     // use `url` here inside the callback because it's asynchronous!
 
-    // }).then(() => {
-    //     console.log(url);
-    // })
+  // }).then(() => {
+  //     console.log(url);
+  // })
 
-    // var name = document.querySelector("input");
-    // console.log(name);
-
+  // var name = document.querySelector("input");
+  // console.log(name);
 });
-
-
-
-
-
-
 
 // tests and fails ------------------------------------------------------------------------
 
@@ -78,10 +229,9 @@ botao.addEventListener("click", function(event){
 // }
 // console.log(country)
 
-
 // var filter = document.querySelector("#search-text");
 // filter.addEventListener("input", function(){
-//     console.log(this.value+"%%%%%%%%%%%%%%%%%%%%%%%%%%%"); 
+//     console.log(this.value+"%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 //     let filteredText = [1111111];
 //     let inputText = filter;
 //     console.log(inputText+"&&&&&&&&&&&&&&")
@@ -93,3 +243,66 @@ botao.addEventListener("click", function(event){
 //     }
 // console.log(filteredText+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 // });
+
+// chrome.runtime.onMessage.addListener(function (request, sender) {
+//   if (request.action == 'getSource') {
+//     message.innerText = request.source;
+//   }
+// });
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  console.log('request', request);
+  var message = document.querySelector('#message');
+  if (request.action == 'getSource') {
+    this.pageSource = request.source;
+    // var teste = this.pageSource.match(/<title[^>]*>([^<]+)<\/title>/)[1];
+    // alert(title);
+    // message.innerText = teste;
+
+    let parser = new DOMParser();
+    let document = parser.parseFromString(this.pageSource, 'text/html');
+    let title = document.querySelector('input[name="q"]').value;
+
+    message.innerText = title;
+  }
+});
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  chrome.tabs.executeScript(tabs[0].id, {
+    code: 'var s = document.documentElement.outerHTML; chrome.runtime.sendMessage({action: "getSource", source: s});',
+  });
+});
+
+function onWindowLoad() {
+  var message = document.querySelector('#message');
+
+  // chrome.tabs.executeScript(
+  //   null,
+  //   {
+  //     file: 'getPagesSource.js',
+  //   },
+  //   function () {
+  //     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+  //     if (chrome.runtime.lastError) {
+  //       message.innerText =
+  //         'There was an error injecting script : \n' +
+  //         chrome.runtime.lastError.message;
+  //     }
+  //   }
+  // );
+  chrome.tabs.executeScript(
+    null,
+    {
+      code: 'var s = document.documentElement.outerHTML; chrome.runtime.sendMessage({action: "getSource", source: s});',
+    },
+    function () {
+      if (chrome.runtime.lastError) {
+        message.innerText =
+          'There was an error injecting script : \n' +
+          chrome.runtime.lastError.message;
+      }
+    }
+  );
+}
+
+window.onload = onWindowLoad;
